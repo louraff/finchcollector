0f49579f86a004e6743f7cf9ae506fc5ae3086fa
Louise Raffray <raffraylouise@gmail.com>
2023-06-16 15:11:09 +0100

    routed feeding form

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 from .forms import FeedingForm
@@ -21,8 +21,20 @@ def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
-    'finch': finch, 'feeding_form' : feeding_form
+    'finch': finch, 'feeding_form': feeding_form
   })
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
 
 
 class FinchCreate(CreateView):
