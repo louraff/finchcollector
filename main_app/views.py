222fd3ceaeb120740dc7b341904d7647a46e1efb
Louise Raffray <raffraylouise@gmail.com>
2023-06-16 14:50:06 +0100

    added CRUD functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Create your views here.
@@ -15,3 +15,21 @@ def finches_index(request):
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', {
+    'finch': finch
+  })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
